1739600b6099279bdd9cc1dd5f9d3152e6353848
liuxf <liuxf@taofen8.com>
2018-03-12 16:09:48 +0800

    提现处理

--- src/main/java/com/github/binarywang/demo/wechat/mapper/AlipayTradeMoneyMapper.java
+++ src/main/java/com/github/binarywang/demo/wechat/mapper/AlipayTradeMoneyMapper.java
@@ -1,7 +1,10 @@
 package com.github.binarywang.demo.wechat.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.github.binarywang.demo.wechat.bean.AlipayTradeMoney;
 /**
@@ -11,7 +14,12 @@ public interface AlipayTradeMoneyMapper {
 
 
 	@Insert("insert into oversea.alipay_trade_money(type, user_id,alipay_account,real_name,coupon_value,status,gmt_create,gmt_modified) values(#{type}, #{userId}, #{alipayAccount}, #{realName},#{couponValue},0, now(),now()")
-	@Options(useGeneratedKeys=true,keyProperty="id")
 	public int add(AlipayTradeMoney alipayTradeMoney);
+	
+	@Select("SELECT * FROM oversea.alipay_trade_money WHERE status = 2")
+	public List<AlipayTradeMoney> getAlipayTradeMoneyList();
+	
+	@Update("UPDATE oversea.alipay_trade_money SET status = 1 WHERE id = #{id}")
+	public int update(Long id);
 
 }
--- src/main/java/com/github/binarywang/demo/wechat/scheduled/PushOrder.java
+++ src/main/java/com/github/binarywang/demo/wechat/scheduled/PushOrder.java
@@ -1,5 +1,8 @@
 package com.github.binarywang.demo.wechat.scheduled;
 
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -10,8 +13,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import com.github.binarywang.demo.wechat.bean.AlipayTradeMoney;
+import com.github.binarywang.demo.wechat.bean.MiniIncomeDetail;
 import com.github.binarywang.demo.wechat.bean.MiniOrder;
+import com.github.binarywang.demo.wechat.mapper.AlipayTradeMoneyMapper;
 import com.github.binarywang.demo.wechat.mapper.MiniOrderMapper;
+import com.github.binarywang.demo.wechat.service.MiniIncomeDetailService;
 
 @Configuration
 @EnableScheduling
@@ -20,6 +27,10 @@ public class PushOrder {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private MiniOrderMapper miniOrderMapper;
+	@Autowired
+	private AlipayTradeMoneyMapper alipayTradeMoneyMapper;
+	@Autowired
+	private MiniIncomeDetailService miniIncomeDetailService;
 
 
     @Scheduled(cron = "0 0/1 * * * ?") // 每1分钟执行一次
@@ -39,6 +50,28 @@ public class PushOrder {
     			//大于30分钟
     			if((new Date().getTime()-miniOrder.getGmtCreate().getTime())/1000/60>30){
     				miniOrderMapper.updateStatus(miniOrder.getId(), 1);
+    			}
+    		}
+        
+    }
+    
+    @Scheduled(cron = "2 * 0/1 * * ?") // 每1小时执行一次
+    public void tradeMoney() {
+    		List<AlipayTradeMoney> alipayTradeMoneys = alipayTradeMoneyMapper.getAlipayTradeMoneyList();
+    		for(AlipayTradeMoney alipayTradeMoney:alipayTradeMoneys) {
+    			int num = alipayTradeMoneyMapper.update(alipayTradeMoney.getId());
+    			if(num>0) {
+    				MiniIncomeDetail miniIncomeDetail = new MiniIncomeDetail();
+    				BigDecimal b1 = new BigDecimal(alipayTradeMoney.getCouponValue());
+    			    BigDecimal b2 = new BigDecimal(100);
+    				miniIncomeDetail.setIncome(String.valueOf(b1.divide(b2, 2, 4)));
+    				miniIncomeDetail.setMiniUserId(alipayTradeMoney.getUser_id());
+    				DateFormat ymdhmsFormat = new SimpleDateFormat("yyMMddHHmmssSSS");
+    				String dateStr = ymdhmsFormat.format(new Date());
+    				miniIncomeDetail.setOrder_no(dateStr);
+    				miniIncomeDetail.setTitle("申请提现");
+    				miniIncomeDetail.setType(3);
+    				miniIncomeDetailService.add(miniIncomeDetail);
     			}
     		}
         
