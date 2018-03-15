83855522a5115a469d6ca8f4922c9821cf9630a6
liuxf <liuxf@taofen8.com>
2018-03-15 09:54:11 +0800

    bug fix

--- src/main/java/com/github/binarywang/demo/wechat/mapper/AccountMapper.java
+++ src/main/java/com/github/binarywang/demo/wechat/mapper/AccountMapper.java
@@ -16,17 +16,17 @@ import com.github.binarywang.demo.wechat.bean.OrderAccount;
  */
 public interface AccountMapper {
 
-	@Select("SELECT * FROM automan.order_account_new WHERE account_id = #{accoutId}")
-	OrderAccount getAccoutIdByAccoutId(@Param("accoutId") Integer accoutId);
+	@Select("SELECT * FROM automan.order_account_new WHERE account_id = #{accountId}")
+	OrderAccount getAccoutIdByAccoutId(@Param("accountId") Integer accountId);
 
 	@Insert("insert into automan.order_account_new(account_type,pay_account, login_pwd,status) values(#{orderAccount.accountType},#{orderAccount.payAccount}, #{orderAccount.loginPwd}, #{orderAccount.status})")
-	@Options(useGeneratedKeys=true,keyProperty="orderAccount.id")
+	@Options(useGeneratedKeys=true,keyProperty="orderAccount.accountId")
 	public int add(@Param("orderAccount") OrderAccount orderAccount);
 
 	@Select("SELECT * FROM automan.order_account_new WHERE account_source = #{accountSource} and account_type = #{accountType}")
 	List<OrderAccount> getOrderAccountByAccountSource(@Param("accountSource")String accountSource, @Param("accountType") String accountType);
 	
-	@Update("UPDATE automan.order_account_new SET pay_account = #{orderAccount.payAccount} , login_pwd = #{orderAccount.loginPwd} WHERE account_id = #{orderAccount.accoutId}")
+	@Update("UPDATE automan.order_account_new SET pay_account = #{orderAccount.payAccount} , login_pwd = #{orderAccount.loginPwd} WHERE account_id = #{orderAccount.accountId}")
 	public int updateOrderAccount(@Param("orderAccount") OrderAccount orderAccount);
 	
 	@Select("SELECT * FROM automan.order_account_new WHERE account_source = #{accountSource}")
